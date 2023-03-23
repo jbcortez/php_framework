f77d5bf938f2e93663ccda22ce7dc8658e9a4d8a
Justin Cortez <jcortez@sierrainstruments.com>
2023-03-23 13:14:26 -0700

    finished core framework

--- framework/app/controllers/Pages.php
+++ framework/app/controllers/Pages.php
@@ -1,15 +1,21 @@
 <?php
   class Pages extends Controller {
+    private $postModel;
+
     public function __construct() {
-      
+      $this->postModel = $this->model('Post');
     }
 
     public function index() {
-      $data = ['title' => 'Welcome'];
+      $posts = $this->postModel->getPosts();
+      
+      $data = ['title' => 'Welcome', 'posts' => $posts];
+
       $this->view('pages/index', $data);
     }
 
     public function about() {
-      $this->view('pages/about');
+      $data = ['title' => 'About'];
+      $this->view('pages/about', $data);
     }
   }
