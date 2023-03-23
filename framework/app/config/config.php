7ba8b7484d3129d779eaa1aefb16265ab9246817
Justin Cortez <jcortez@sierrainstruments.com>
2023-03-23 11:41:57 -0700

    core framework

---
+++ framework/app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL root
+  define('URLROOT', 'http://localhost/php_oop/framework');
+  // Site name
+  define('SITENAME', 'TraversyMVC');
